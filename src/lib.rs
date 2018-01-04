7d1a72e08f2c1256ba3413dd1c49f2c98c11a35c
Rico A. Beti <rico.beti@silentbyte.com>
2018-01-04 23:27:14 +0800

    Add Travis CI

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+//!
+//! Rust name_of! Macro
+//!
+//! MIT License
+//! Copyright (c) 2018 SilentByte <https://silentbyte.com/>
+//!
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn travis_test() {
+        //
+    }
+}
+
