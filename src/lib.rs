b11a1745062496321cd7470d458c9c844e6773ef
Rico A. Beti <rico.beti@silentbyte.com>
2018-01-05 01:16:33 +0800

    Add alternative macro name_of_type!()

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,24 @@
 /// in the current context, the macro will cause a compilation error.
 /// This macro is mainly intended for debugging purposes and to improve
 /// the refactoring experience compared to `stringify!()`.
+///
+/// # Examples
+///
+/// ```
+/// // Bindings
+/// let text = "Hello, World!";
+/// println!("Variable `{}` holds `{}`.", name_of!(text), text);
+///
+/// // Functions
+/// fn greet() { }
+/// println!("Function is called `{}`.", name_of!(greet));
+///
+/// // Types & Fields
+/// struct TestStruct { test_field: i32 }
+/// println!("Struct is called `{}`.", name_of!(type TestStruct));
+/// println!("Field is called `{}`.", name_of!(test_field for TestStruct));
+/// println!("Standard Types: `{}`.", name_of!(i32));
+/// ```
 #[macro_export]
 macro_rules! name_of {
     // Covers Bindings
@@ -18,16 +36,34 @@ macro_rules! name_of {
         stringify!($n)
     }};
 
-    // Covers Structs
+    // Covers Types
     (type $t: ty) => {{
-        || { let _: $t; };
-        stringify!($t)
+        name_of_type!($t)
     }};
 
     // Covers Struct Fields
     ($n: ident for $t: ty) => {{
         |f: $t| { let _ = &f.$n; };
         stringify!($n)
+    }};
+}
+
+/// Alternative for the `name_of!(type T)` macro specifically for types.
+///
+/// # Examples
+///
+/// ```
+/// // Alternative for Types
+/// struct TestStruct { test_field: i32 }
+/// println!("Struct is called `{}`.", name_of_type!(TestStruct));
+/// println!("Struct is called `{}`.", name_of_type!(i32));
+/// ```
+#[macro_export]
+macro_rules! name_of_type {
+    // Covers Types
+    ($t: ty) => {{
+        || { let _: $t; };
+        stringify!($t)
     }};
 }
 
@@ -57,14 +93,26 @@ mod tests {
     }
 
     #[test]
+    fn name_of_type() {
+        assert_eq!(name_of!(type i32), "i32");
+        assert_eq!(name_of_type!(i32), "i32");
+    }
+
+    #[test]
     fn name_of_struct() {
         assert_eq!(name_of!(type TestStruct), "TestStruct");
+        assert_eq!(name_of_type!(TestStruct), "TestStruct");
     }
 
     #[test]
     fn name_of_generic_struct() {
         assert_eq!(
             name_of!(type TestGenericStruct<i32>),
+            "TestGenericStruct<i32>"
+        );
+
+        assert_eq!(
+            name_of_type!(TestGenericStruct<i32>),
             "TestGenericStruct<i32>"
         );
     }
